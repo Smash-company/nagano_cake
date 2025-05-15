f244846bbd9f19a4ff62d595b064dfe11c7ccbaa
TsuyoshiTaniguchi <202054797+TsuyoshiTaniguchi@users.noreply.github.com>
2025-05-15 11:01:46 +0000

    models commit

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,28 @@
 class Admin::OrdersController < ApplicationController
+  before_action :authenticate_admin!
+
   def show
+    @order = Order.find(params[:id])
+    #@orders = Orders.all 必要かもしれない
+    @order_details = @order.order_details
   end
 
   def update
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
+    @order.update(order_params)
+
+    if @order.status == 'comfirmed_payment'
+      @order_details.update_all(making_status: 1)
+    else
+      redirect_to admin_order_path(@order)
+    end
   end
+
+  private
+
+  def order_params
+    params.require(:order).permit(:customer_id, :postal_code, :adress, :name, :postage, :total_amount, :payment_method, :status)
+  end
+
 end
