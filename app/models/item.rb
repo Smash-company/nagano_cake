f8923e158199970d5dc91f10c1f9056285e9a9cb
TsuyoshiTaniguchi <202054797+TsuyoshiTaniguchi@users.noreply.github.com>
2025-05-16 03:21:43 +0000

    admin_orders commit

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,11 +4,9 @@ class Item < ApplicationRecord
   has_many :order_details, dependent: :destroy
   belongs_to :genre
 
-
   has_one_attached :image
 
   validates :name, presence: true
-
   validates :image, presence: true
   validates :opinion, presence: true
   validates :price, presence: true
@@ -16,18 +14,20 @@ class Item < ApplicationRecord
 
   TAX_RATE = 1.1
 
-# 税込価格の表記
+ # 税込価格の表記
+
   def add_tax_price
     (self.price * TAX_RATE).floor
   end
-# 商品画像の記述
+
+ # 商品画像の記述
 
   #def get_image(width, height)
     #image.variant(resize: "#{width}x#{height}!").processed
   #end
 
-
   def get_image(width, height)
     image.variant(resize: "#{width}x#{height}!").processed
   end
+
 end
