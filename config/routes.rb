55d3f860e2e188bba2543d9cf29a8e18a53bd54f
EC2 Default User <202753780+Ice-wolf001@users.noreply.github.com>
2025-05-19 04:42:36 +0000

    Genre Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,38 +1,43 @@
 Rails.application.routes.draw do
-  devise_for :admins, controllers: {
-    sessions: 'admins/sessions'
+  devise_for :admin, controllers: {
+    sessions: 'admin/sessions'
   }
-  devise_for :customers
+  devise_for :customers, controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "public/homes#top"
   get '/about' => 'public/homes#about', as: 'about'
 
   namespace :admin do
     resources :items, only: [:index, :new, :create, :show, :edit, :update]
-    resources :genres, only: [:index, :show, :edit, :update] 
+    resources :genres, only: [:index, :create, :edit, :update] 
     resources :customers, only: [:index, :show, :edit, :update] 
     resources :orders, only: [:show, :update] 
     resources :order_details, only: [:update] 
-    root to: 'homes#top'
+    get '/' => 'homes#top'
   end
 
   scope module: :public do
-    resources :items, only: [:index, :show]
-
     get '/customers/mypage' => 'customers#show'
     get '/customers/information/edit' => 'customers#edit'
     patch '/customers/information' => 'customers#update'
     get '/customers/check' => 'customers#check'
     patch '/customers/withdraw' => 'customers#withdraw'
 
-    get '/cart_items' => 'cart_items#index'
-    patch '/cart_items/:id' => 'cart_items#update'
-    delete '/cart_items/:id' => 'cart_items#destroy'
     delete '/cart_items/destroy_all' => 'cart_items#destroy_all'
-    post '/cart_items' => 'cart_items#create'
 
-    resources :orders, only: [:new, :check, :confirm, :create, :index, :show] 
+    post '/orders/check' => 'orders#check'
+    get '/orders/confirm' => 'orders#confirm'
+
+    get '/search' => 'items#search'
+
+    resources :items, only: [:index, :show]
+    resources :cart_items, only: [:index, :update, :destroy, :create]
+    resources :orders, only: [:new, :create, :index, :show] 
     resources :addresses, only: [:index, :edit, :create, :update, :destroy] 
+    resources :genres, only: [:show]
   end
   
 end
